4590ae9bddbe685c45c524377be0e756d4b6102e
cheerlong <cheerlong@LENOVO-PC>
2016-06-03 14:56:55 +0800

    te

--- wechat/Controllers/Xinghe1Controller.cs
+++ wechat/Controllers/Xinghe1Controller.cs
@@ -12,51 +12,53 @@ namespace wechat.Controllers
         // GET: Xinghe1
         public ActionResult Index(string code)
         {
-            //Utils.WeHelper.appid = "wx7da1972d9ac96dc1";
-            //Utils.WeHelper.secret = "b2733c2b8663e5c325ef0f0c41e9197e";
-            //if (!string.IsNullOrWhiteSpace(code))
-            //{
-            //    Utils.WeHelper.code = code;
+            Utils.WeHelper.appid = "wx7da1972d9ac96dc1";
+            Utils.WeHelper.secret = "b2733c2b8663e5c325ef0f0c41e9197e";
+            if (!string.IsNullOrWhiteSpace(code))
+            {
+                Utils.WeHelper.code = code;
+                try
+                {
+                    string strjson = Utils.WeHelper.GetUserInfo();
 
-            //    string strjson = Utils.WeHelper.GetUserInfo();
+                //string strjson = "{\"openid\":\"oQOyyv - MdUWSgP8_Smoh2S_6 - 1I0\",\"nickname\":\"白鹤\",\"sex\":1,\"language\":\"zh_CN\",\"city\":\"长宁\",\"province\":\"上海\",\"country\":\"中国\",\"headimgurl\":\"http://wx.qlogo.cn/mmopen/78EkX665csCmkBmDBDSYTDCmZdvlMDqCX7wYTLcHeeKNeLicSS5ic2fDAYpeTqicaqhF8Iw9Rp9d6hegynMHC7tPMWLRnqMvNicn/0\",\"privilege\":[]}";
+                Utils.Log.Info("userin", strjson);
+                LitJson.JsonData jd = LitJson.JsonMapper.ToObject(strjson);
 
-            //    //string strjson = "{\"openid\":\"oQOyyv - MdUWSgP8_Smoh2S_6 - 1I0\",\"nickname\":\"白鹤\",\"sex\":1,\"language\":\"zh_CN\",\"city\":\"长宁\",\"province\":\"上海\",\"country\":\"中国\",\"headimgurl\":\"http://wx.qlogo.cn/mmopen/78EkX665csCmkBmDBDSYTDCmZdvlMDqCX7wYTLcHeeKNeLicSS5ic2fDAYpeTqicaqhF8Iw9Rp9d6hegynMHC7tPMWLRnqMvNicn/0\",\"privilege\":[]}";
-            //    Utils.Log.Info("userin", strjson);
-            //    LitJson.JsonData jd = LitJson.JsonMapper.ToObject(strjson);
-
-
-
-
-            //    try
-            //    {
-            //        var wu = db.Question.SingleOrDefault(w => w.openid.Equals(jd["openid"]));
-
-            //        if (wu == null)
-            //        {
+              string openid=  jd["openid"].ToString();
 
 
-            //            ViewBag.openid = jd["openid"].ToString();
-            //            return View();
-            //        }
-            //        else
-            //        {
-            //            return RedirectToAction("Thanks");
-            //        }
+                
+                    var wu = db.Question.SingleOrDefault(w => w.openid.Equals(openid));
+
+                    if (wu == null)
+                    {
 
 
-            //    }
-            //    catch (Exception ex)
-            //    {
+                        ViewBag.openid = openid;
+                      
+                    }
+                    else
+                    {
+                        return RedirectToAction("Thanks");
+                    }
 
-            //        Utils.Log.Error("userAdd", ex.Message);
-            //    }
-            //}
-            //else
-            //{
-            //    Response.Redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + Utils.WeHelper.appid + "&redirect_uri=http://mvc.cjoy.cn/Xinghe1/index&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect");
-            //}
 
-            ViewBag.openid = "bbbbb";
+                }
+                catch (Exception ex)
+                {
+
+                    Utils.Log.Error("userAdd", ex.Message);
+
+                  
+                }
+            }
+            else
+            {
+                Response.Redirect("https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + Utils.WeHelper.appid + "&redirect_uri=http://mvc.cjoy.cn/Xinghe1/index&response_type=code&scope=snsapi_base&state=STATE#wechat_redirect");
+            }
+
+        
             return View();
         }
 
