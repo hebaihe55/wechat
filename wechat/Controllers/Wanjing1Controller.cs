422a57e3777eb085a983688dad7b3cdb7b84fc21
cheerlong <cheerlong@LENOVO-PC>
2016-06-19 08:49:57 +0800

    11

---
+++ wechat/Controllers/Wanjing1Controller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wechat.Controllers
+{
+    public class Wanjing1Controller : Controller
+    {
+        // GET: Wanjing
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult tijiao()
+        {
+            return View();
+        }
+        public ActionResult thank()
+        {
+            return View();
+        }
+        public ActionResult activeend()
+        {
+            return View();
+        }
+        public ActionResult liuliangzsend()
+        {
+            return View();
+        }
+    }
+
+}
