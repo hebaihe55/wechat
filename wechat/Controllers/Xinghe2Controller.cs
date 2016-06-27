949c1018a6abaf818f53b677d8e0ecec447da12f
cheerlong <cheerlong@LENOVO-PC>
2016-06-27 16:15:57 +0800

    11

---
+++ wechat/Controllers/Xinghe2Controller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wechat.Controllers
+{
+    public class Xinghe2Controller : Controller
+    {
+        // GET: Xinghe2
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult rule()
+        {
+            return View();
+        }
+        public ActionResult thank()
+        {
+            return View();
+        }
+    }
+}
