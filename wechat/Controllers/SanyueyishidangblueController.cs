145751d3a7a641cd9d16ea91d7c5eb94626f5934
cheerlong <cheerlong@LENOVO-PC>
2016-07-01 13:44:17 +0800

    11

---
+++ wechat/Controllers/SanyueyishidangblueController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wechat.Controllers
+{
+    public class SanyueyishidangblueController : Controller
+    {
+        // GET: Sanyueyishidangblue
+        public ActionResult subinfo()
+        {
+            return View();
+        }
+        public ActionResult thank()
+        {
+            return View();
+        }
+    }
+}
