585508e22bafffe62aee952ac8e02917bed683b8
cheerlong <cheerlong@LENOVO-PC>
2016-07-04 17:19:08 +0800

    11

---
+++ wechat/Content/Qilingpijiu/js/main.js
@@ -0,0 +1,93 @@
+﻿var isdrag = false;
+var tx, x, ty, y;
+$(function () {
+    document.getElementsByClassName("dragme")[0].addEventListener('touchend', function () {
+        sdrag = false;
+    });
+    document.getElementsByClassName("dragme")[0].addEventListener('touchstart', selectmouse);
+    document.getElementsByClassName("dragme")[0].addEventListener('touchmove', movemouse);
+
+});
+function movemouse(e) {
+
+    if (isdrag) {
+
+        var n = tx + e.touches[0].pageX - x;
+        var m = ty + e.touches[0].pageY - y;
+        var predivwidth = $(".dragme").parent().width();//393
+        var predivheight = $(".dragme").parent().height();//424
+        // alert(predivwidth + "||" + predivheight)
+        if (n <= -predivwidth) {
+            n = -predivwidth;
+        } else if (n >= predivwidth) {
+            n = predivwidth - 200;
+        }
+        if (m <= -predivheight) {
+            m = -predivheight;
+        } else if (m >= predivheight) {
+            m = predivheight - 150;
+        }
+
+        $(".dragme").css("left", n);
+
+        $(".dragme").css("top", m);
+
+
+
+        return false;
+    }
+}
+function selectmouse(e) {
+    // alert(123);
+    isdrag = true;
+
+    tx = parseInt(document.getElementsByClassName("dragme")[0].style.left + 0);
+    ty = parseInt(document.getElementsByClassName("dragme")[0].style.top + 0);
+
+    x = e.touches[0].pageX;
+
+    y = e.touches[0].pageY;
+
+    return false;
+}
+
+var str = {
+    inputname:"请输入支付宝账号",
+    telTish: "手机号码有误",
+    zhezhaotemplate: '<div class="zhezhao"></div>',
+    ruletemplate: "<div class='ruleshow'><img src='/Content/yamaha/image/gamerule.jpg'/></div>",
+    closebtnimg: "<div class='closebtn'></div>",
+}
+var alertdialog1 = function (text) {
+    var clientWidth = $(window).width();
+    var clienHeight = $(window).height();
+    var alerttemplate1 = '<div class="alertParent"><div class="text"><span>' + text + '</span></div><input type="button" value="确定" onclick="sure1()" class="surebtn"/></div>'
+    $("body").append(str.zhezhaotemplate);
+    $("body").append(alerttemplate1);
+
+    $(".alertParent").css({
+        "top": (clienHeight / 2 - 400) + "px",
+        "left": (clientWidth / 2 - 224) + "px",
+        "z-index": "3",
+    });
+    return;
+
+}
+var checkform = function () {
+    var user = $("form .user").val();
+    var tel = $("form .tel").val();
+    if (user == null || user == "") {
+        alertdialog1(str.inputname);
+        return false;
+    }
+    var reg = /^1[3|4|5|7|8]\d{9}$/;
+    if (!(reg.test(tel))) {
+        alertdialog1(str.telTish);
+        return false;
+    }
+    return true;
+}
+var sure1 = function () {
+    $(".alertParent").hide();
+    $(".zhezhao").hide();
+}
---
+++ wechat/Controllers/Qilingpijiu1Controller.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wechat.Controllers
+{
+    public class Qilingpijiu1Controller : Controller
+    {
+        // GET: Qilingpijiu1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult hongbaolingwan()
+        {
+            return View();
+        }
+        public ActionResult gamerule()
+        {
+            return View();
+        }
+        public ActionResult thank()
+        {
+            return View();
+        }
+        public ActionResult submitinfo()
+        {
+            return View();
+        }
+        public ActionResult shangchuan()
+        {
+            return View();
+        }
+    }
+}
