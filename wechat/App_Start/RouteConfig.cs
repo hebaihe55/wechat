2c9ea38cbb366c2008d1426e0716f33073a5d91a
cheerlong <cheerlong@LENOVO-PC>
2016-08-23 18:19:31 +0800

    nnn

--- wechat/App_Start/RouteConfig.cs
+++ wechat/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace wechat
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "AdminQL", action = "Index", id = UrlParameter.Optional }
             );
 
           
