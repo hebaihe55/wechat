629c32797e96b668685a09fc8853b6299ed5bd97
cheerlong <cheerlong@LENOVO-PC>
2016-08-24 15:18:00 +0800

    bb

--- wechat/Controllers/Yamaha3Controller.cs
+++ wechat/Controllers/Yamaha3Controller.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using wechat.Models;
 
 namespace wechat.Controllers
 {
     public class Yamaha3Controller : Controller
     {
+        WechatEntities2 db = new WechatEntities2();
         // GET: Yamaha3
         public ActionResult zong()
         {
+
             return View();
         }
         public ActionResult zhishiindex()
@@ -19,8 +22,13 @@ namespace wechat.Controllers
         }
         public ActionResult timu()
         {
-            return View();
+            var ss = (from p in db.yamahaTimu orderby Guid.NewGuid() select p).Take(1).SingleOrDefault();
+            ViewBag.t = ss.timu;
+            ViewBag.a = ss.A;
+            ViewBag.b = ss.B;
+            ViewBag.c = ss.C;
+            ViewBag.d = ss.D;
+            return View(ss );
         }
-
     }
 }
