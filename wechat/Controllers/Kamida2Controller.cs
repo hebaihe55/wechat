66f5773e801aebe9f716bb1d46e44c8b539ee5c3
cheerlong <cheerlong@LENOVO-PC>
2016-09-02 16:17:07 +0800

    11

---
+++ wechat/Controllers/Kamida2Controller.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace wechat.Controllers
+{
+    public class Kamida2Controller : Controller
+    {
+        // GET: Kamida2
+        public ActionResult kami1()
+        {
+            return View();
+        }
+        public ActionResult kami2()
+        {
+            return View();
+        }
+        public ActionResult kami3()
+        {
+            return View();
+        }
+        public ActionResult kami4()
+        {
+            return View();
+        }
+        public ActionResult kami5()
+        {
+            return View();
+        }
+    }
+}
